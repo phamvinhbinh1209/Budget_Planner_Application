53d86c99e628a74330695eafc734d5c4c8aa57cb
phamvinhbinh1209 <phamvinhbinh12092003@gmail.com>
2024-10-21 23:54:52 +0700

    authentication

---
+++ app/index.jsx
@@ -0,0 +1,52 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useEffect } from 'react'
+import {Link, useRouter} from 'expo-router'
+import services from './../utils/services'
+import {client} from './../utils/KindeConfig'
+
+export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    checkUserAuth();
+  }, [])
+
+  /**
+   * Used to check user Is already auth or not
+   */
+
+  const checkUserAuth = async() => {
+    const result = await services.getData('login');
+    if(result!=='true')
+    {
+      router.replace('/login');
+    }
+  }
+
+  const handleLogout = async () => {
+    const loggedOut = await client.logout();
+    if (loggedOut) {
+      await services.storeData('login', 'false');
+      router.replace('/login');
+      // User was logged out
+    }
+  };
+
+  return (
+    <View style= {{
+      marginTop: 20,
+    }}>
+      <Text style={styles.text}>I like money so much!</Text>
+
+      <Button title='Logout' onPress={handleLogout} />
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    text:{
+      fontSize: 20
+    }
+})
---
+++ utils/KindeConfig.jsx
@@ -0,0 +1,7 @@
+import { KindeSDK } from '@kinde-oss/react-native-sdk-0-7x';
+
+export const client = new KindeSDK(
+    'https://budgetplannerappforb.kinde.com', 
+    'exp://192.168.1.4:8081', 
+    'ce2a66b5d72a4813b424bd7dfdbfc505', 
+    'exp://192.168.1.4:8081');
